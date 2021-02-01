6fef4f92eb0c71991b80e4b9611d5225cb617c7a
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-02-01 08:18:19 -0800

    2,1,21

--- sorted_frozen_set.py
+++ sorted_frozen_set.py
@@ -1,5 +1,10 @@
 class SortedFrozenSet:
 
     def __init__(self, items=None):
-        pass
+        self._items = (
+            list(items) if (items is not None)
+            else list()
+        )
 
+    def __contains__(self, item):
+        return item in self._items
